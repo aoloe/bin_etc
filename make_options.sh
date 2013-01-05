c4fa9077d9025772cd87c3003fb6558a44ea1b84
a.l.e <ale@ideale.ch>
2013-01-05 12:03:29 +0100

    ...

---
+++ make_options.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd ~/src/
+rm -rf Scribus
+svn co svn://scribus.net/trunk/Scribus scribus
+cd Scribus
+export PATH='/usr/local/qt-4.4/bin':$PATH
+#/usr/local/cmake/bin/cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr/local/scribus14 -DLIBPODOFO_DIR=/usr/local/podofo/ -DWANT_CAIRO=1 -DWANT_DEBUG=1
+#cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr/local/scribus15 -DLIBPODOFO_DIR=/usr/local/podofo/ -DWANT_DEBUG=1 -DWANT_NOOSG=1
+mkdir build/
+cd build/
+#cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr/local/scribus15 -DWANT_DEBUG=1 -DWANT_NOOSG=1 ..
+#cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr/local/scribus15 -DWANT_DEBUG=1 -DWANT_NOOSG=1 -DWANT_PRIVATE_CAIRO=1 -DWANT_SCRIPTER2=1
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/home/ale/docs/bin/scribus -DWANT_DEBUG=1 -DWANT_NOOSG=1 -DWANT_SCRIPTER2=1 -DWANT_GUI_LANG="en_GB;de;fr;it;en" ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/home/ale/docs/bin/scribus -DWANT_DEBUG=1 -DWANT_GUI_LANG="en_GB;de;fr;it;en" ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/home/ale/docs/bin/scribus_git -DWANT_DEBUG=1 -DWANT_GUI_LANG="en_GB;de;fr;it;en" ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/home/ale/docs/bin/scribus_oif -DWANT_DEBUG=1 -DWANT_NOOSG=1 ..
+svn co svn://scribus.net/branches/Version14x/Scribus scribus14
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/home/ale/docs/bin/scribus14 -DWANT_DEBUG=1 -DWANT_GUI_LANG="en_GB;de;fr;it;en" ..
+make
+sudo rm -rf /usr/local/scribus15~
+sudo mv /usr/local/scribus15 /usr/local/scribus15~
+sudo make install
+
+
+-DWANT_GUI_LANG="en_GB;de;fr;it;en" // to reduce the number of languages for the GUI
